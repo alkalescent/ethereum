fdf680b8c9fe08b2b6670957a2c3b0b102b6a0b8
Krish Suchak <suchak.krish@gmail.com>
2025-02-23 18:41:41 -0500

    almost done with download

--- vpn/download.py
+++ vpn/download.py
@@ -1,5 +1,7 @@
 import os
-import re
+import shutil
+from glob import glob
+from zipfile import ZipFile
 import geoip2.database
 import urllib.request
 from pathlib import Path
@@ -20,13 +22,18 @@ def download_file(url):
     return filename
 
 
-def delete(filename):
-    os.remove(filename)
+def delete(path):
+    if os.path.isfile(path):
+        return os.remove(path)
+    elif os.path.isdir(path):
+        return shutil.rmtree(path)
+    else:
+        raise Exception('Path to delete does not exist.')
 
 
-def multidelete(filenames):
+def multidelete(paths):
     with Pool() as p:
-        return p.map(delete, filenames)
+        return p.map(delete, paths)
 
 
 def geolocate(filename):
@@ -41,15 +48,35 @@ def geolocate(filename):
             return response.city.names['en']
 
 
+def move(cfg):
+    return shutil.move(cfg, CONFIG_DIR)
+
+
 def get_servers():
-    full_path = os.path.realpath(__file__)
-    curr_dir = os.path.dirname(full_path)
-    with open(os.path.join(curr_dir, 'nordvpn.html')) as file:
-        content = file.read()
-        matches = re.findall(
-            r'https:\/\/downloads.nordcdn.com\/configs\/files\/ovpn_tcp\/servers\/us\d+.nordvpn.com.tcp.ovpn', content)
-        with Pool() as p:
-            return p.map(download_file, matches)
+    # full_path = os.path.realpath(__file__)
+    # curr_dir = os.path.dirname(full_path)
+    # with open(os.path.join(curr_dir, 'nordvpn.html')) as file:
+    #     content = file.read()
+    #     matches = re.findall(
+    #         r'https:\/\/downloads.nordcdn.com\/configs\/files\/ovpn_tcp\/servers\/us\d+.nordvpn.com.tcp.ovpn', content)
+    #     with Pool() as p:
+    #         return p.map(download_file, matches)
+
+    # NEW
+    VPN_DIR = 'ovpn'
+    TCP_DIR = 'ovpn_tcp'
+    VPN_EXT = 'zip'
+    ZIP_FN = f'{VPN_DIR}.{VPN_EXT}'
+    ZIP_PATH = os.path.join(CONFIG_DIR, ZIP_FN)
+    VPN_DIR = os.path.join(CONFIG_DIR, VPN_DIR)
+    download_file(
+        f'https://downloads.nordcdn.com/configs/archives/servers/{ZIP_FN}')
+    with ZipFile(ZIP_PATH, 'r') as zf:
+        zf.extractall(VPN_DIR)
+    # cfgs = os.listdir(os.path.join(VPN_DIR, TCP_DIR))
+    cfgs = glob(os.path.join(VPN_DIR, TCP_DIR, '*.tcp.ovpn'))
+    with Pool() as p:
+        return p.map(move, cfgs)
 
 
 def multigeolocate(servers):
@@ -59,11 +86,11 @@ def multigeolocate(servers):
 
 if __name__ == '__main__':
     Path(CONFIG_DIR).mkdir(parents=True, exist_ok=True)
-    db_path = download_db()
+    # db_path = download_db()
     servers = get_servers()
-    locations = multigeolocate(servers)
-    far_servers = [server for server, location in zip(
-        servers, locations
-    ) if location not in {'Miami', 'Atlanta'}]
-    multidelete(far_servers)
-    delete(DB_PATH)
+    # locations = multigeolocate(servers)
+    # far_servers = [server for server, location in zip(
+    #     servers, locations
+    # ) if location not in {'Miami', 'Atlanta'}]
+    # multidelete(far_servers)
+    # delete(DB_PATH)
