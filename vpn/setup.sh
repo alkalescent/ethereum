4c9c49691d28143f583a50bf4b2373d0446c441d
Krish Suchak <suchak.krish@gmail.com>
2025-02-22 23:16:15 -0500

    progress

--- vpn/setup.sh
+++ vpn/setup.sh
@@ -6,6 +6,8 @@ VPN="${VPN:-false}"
 
 if [[ "${VPN}" = "true" ]]
 then
-    apt-get update && apt-get install -y openvpn ca-certificates
+    apt-get update && apt-get install -y ca-certificates
+    # sudo wget https://downloads.nordcdn.com/configs/archives/servers/ovpn.zip
+    sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
     python3 vpn/download.py
 fi
