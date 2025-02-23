4dd1fd350ec07968c88356322d40e212f58e2620
Krish Suchak <suchak.krish@gmail.com>
2025-02-23 13:01:17 -0500

    comment out

--- vpn/setup.sh
+++ vpn/setup.sh
@@ -9,8 +9,8 @@ then
     apt-get update && apt-get install -y ca-certificates
     yes | sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
     echo NordVPN installed
-    /etc/init.d/nordvpn start
-    echo NordVPN service started
+    # /etc/init.d/nordvpn start
+    # echo NordVPN service started
     # nordvpn login --token "${NORDVPN}"
     # echo Logged into NordVPN
 fi
