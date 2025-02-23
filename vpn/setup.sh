80b44de0da661f7144f5ce8958da980bcffd7567
Krish Suchak <suchak.krish@gmail.com>
2025-02-22 23:16:47 -0500

    nordvpn support article

--- vpn/setup.sh
+++ vpn/setup.sh
@@ -7,6 +7,7 @@ VPN="${VPN:-false}"
 if [[ "${VPN}" = "true" ]]
 then
     apt-get update && apt-get install -y ca-certificates
+    # https://support.nordvpn.com/hc/en-us/articles/20164827795345-Connect-to-NordVPN-using-Linux-Terminal
     # sudo wget https://downloads.nordcdn.com/configs/archives/servers/ovpn.zip
     sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
     python3 vpn/download.py
