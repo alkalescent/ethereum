f23c30be14a4f0b49fb41b4cd00b57790955113c
Krish Suchak <suchak.krish@gmail.com>
2025-08-12 10:49:09 -0400

    add slip39 assertion

--- cli/slip39/__init__.py
+++ cli/slip39/__init__.py
@@ -1,21 +1,28 @@
-import slip39
+import slip39 as s
 
 
 class SLIP39:
     """SLIP39 implementation for generating and reconstructing mnemonic phrases."""
 
     def __init__(self):
-        self.mnemo = slip39.recovery.Mnemonic()
+        self.mnemo = s.recovery.Mnemonic()
 
     def deconstruct(self, mnemo: str) -> tuple[str, str]:
         """Deconstruct a mnemo into its shares."""
-        _, shares = slip39.api.create("LEDGER", 1, {"KEYS": (
+        _, shares = s.api.create("LEDGER", 1, {"KEYS": (
             2, 3)}, mnemo, using_bip39=True).groups["KEYS"]
         return shares
 
     def reconstruct(self, shares: list[str]) -> str:
         """Reconstruct multiple shares into a mnemo."""
-        entropy = slip39.recovery.recover(
+        entropy = s.recovery.recover(
             shares, using_bip39=True, as_entropy=True)
         mnemo = self.mnemo.to_mnemonic(entropy)
         return mnemo
+
+
+slip39 = SLIP39()
+# Generate a 24 word mnemonic
+mnemo = slip39.mnemo.generate(256)
+# Check that the reconstruction is correct
+assert mnemo == slip39.reconstruct(slip39.deconstruct(mnemo))
