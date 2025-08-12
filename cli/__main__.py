d5e0f90ac9b1c3a745fb0500a0a79aaf663d36ea
Krish Suchak <suchak.krish@gmail.com>
2025-08-12 03:53:30 -0400

    refactor

--- cli/__main__.py
+++ cli/__main__.py
@@ -9,8 +9,8 @@ class CLI:
         self.bip39 = BIP39()
         self.slip39 = SLIP39()
 
-    def get_seed(self, filename: str) -> bytes:
-        """Get the seed from a file."""
+    def get_mnemo(self, filename: str) -> bytes:
+        """Get the mnemo from a file."""
         with open(filename, "r") as f:
             return f.read()
 
@@ -28,21 +28,21 @@ class CLI:
 
 
 cli = CLI()
-seed = cli.get_seed("seed.txt")
-print("Mnemonic:", seed)
-eth = cli.bip39.eth(seed)
-print("Eth Addr:", eth)
-bip_one, bip_two = cli.bip39.deconstruct(seed)
+mnemo = cli.get_mnemo("seed.txt")
+print("Mnemonic:", mnemo)
+addr = cli.bip39.eth(mnemo)
+print("Eth Addr:", addr)
+bip_one, bip_two = cli.bip39.deconstruct(mnemo)
 print("Deconstructed:", bip_one, bip_two)
 slip_one = cli.slip39.deconstruct(bip_one)
 print("SLIP39 Shares:", slip_one)
-bip_one_reconstructed = cli.slip39.reconstruct(slip_one[0], slip_one[1])
+bip_one_reconstructed = cli.slip39.reconstruct(slip_one)
 print("Reconstructed BIP39:", bip_one_reconstructed)
 slip_two = cli.slip39.deconstruct(bip_two)
 print("SLIP39 Shares:", slip_two)
-bip_two_reconstructed = cli.slip39.reconstruct(slip_two[0], slip_two[1])
+bip_two_reconstructed = cli.slip39.reconstruct(slip_two)
 print("Reconstructed BIP39:", bip_two_reconstructed)
-seed_reconstructed = cli.bip39.reconstruct(
+mnemo_reconstructed = cli.bip39.reconstruct(
     [bip_one_reconstructed, bip_two_reconstructed])
-print("Reconstructed Mnemonic:", seed_reconstructed)
-print("Match:", seed == seed_reconstructed)
+print("Reconstructed Mnemonic:", mnemo_reconstructed)
+print("Match:", mnemo == mnemo_reconstructed)
--- cli/slip39/__init__.py
+++ cli/slip39/__init__.py
@@ -10,13 +10,12 @@ class SLIP39:
     def deconstruct(self, mnemo: str) -> tuple[str, str]:
         """Deconstruct a mnemo into its shares."""
         _, shares = slip39.api.create("LEDGER", 1, {"KEYS": (
-            2, 3)}, seed, using_bip39=True).groups["KEYS"]
+            2, 3)}, mnemo, using_bip39=True).groups["KEYS"]
         return shares
 
-    def reconstruct(self, seed_one: str, seed_two: str) -> str:
-        """Reconstruct multiple shares into a seed."""
-        shares = [seed_one, seed_two]
-        seed = slip39.recovery.recover(
+    def reconstruct(self, shares: list[str]) -> str:
+        """Reconstruct multiple shares into a mnemo."""
+        entropy = slip39.recovery.recover(
             shares, using_bip39=True, as_entropy=True)
-        reconstructed = self.mnemo.to_mnemonic(seed)
-        return reconstructed
+        mnemo = self.mnemo.to_mnemonic(entropy)
+        return mnemo
