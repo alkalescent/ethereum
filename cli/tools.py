8fd84992ed627185f52d87d2db53a4901d52a765
Krish Suchak <suchak.krish@gmail.com>
2025-08-13 22:24:07 -0400

    use sys attr

--- cli/tools.py
+++ cli/tools.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import requests
 from hdwallet import HDWallet
 from hdwallet.symbols import ETH
@@ -16,10 +17,9 @@ class BIP39:
         FILE = os.path.join(DIR, FILENAME)
         self.FILE = FILE
         self.mnemo = Mnemonic()
-        # TODO: set env var for pyinstaller
-        # if not env var set, download words
-        # self.words = self._get()
-        # self.map = self._make(self.words)
+        if not hasattr(sys, '_MEIPASS'):
+            self.words = self._get()
+            self.map = self._make(self.words)
 
     def _download(self) -> None:
         """Download the BIP39 words file."""
