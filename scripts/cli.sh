39b147004ea06ea8fce0ad2b56e81e38b058e8e6
Krish Suchak <suchak.krish@gmail.com>
2025-12-23 19:47:47 -0500

    rename

--- scripts/cli.sh
+++ scripts/cli.sh
@@ -3,4 +3,4 @@
 set -eu
 cd cli
 SITE_PACKAGES=$(uv run python3 -c "import sysconfig; print(sysconfig.get_path('purelib'))")
-uv run pyinstaller --onefile --name cli --distpath ../dist --add-data="$SITE_PACKAGES/shamir_mnemonic/wordlist.txt:./shamir_mnemonic" __main__.py
+uv run pyinstaller --onefile --name cli --distpath ../dist --add-data="$SITE_PACKAGES/shamir_mnemonic/wordlist.txt:./shamir_mnemonic" cli.py
