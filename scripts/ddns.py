021e16d5ea22cbd7901746a31ba8f50164ee623d
Krish Suchak <suchak.krish@gmail.com>
2026-01-12 00:56:45 -0500

    updated scripts for lint

--- scripts/ddns.py
+++ scripts/ddns.py
@@ -1,3 +1,5 @@
+"""Dynamic DNS updater for Route53."""
+
 import os
 from time import sleep
 
@@ -5,46 +7,56 @@ import boto3
 import requests
 from dotenv import find_dotenv, load_dotenv
 
-load_dotenv(find_dotenv('config.env'))
+load_dotenv(find_dotenv("config.env"))
 
 TTL = 3600
 
 
-def get_ip():
-    response = requests.get('https://4.ident.me')
+def get_ip() -> str | None:
+    """Fetch the current public IP address."""
+    response = requests.get("https://4.ident.me", timeout=10)
     if response.ok:
         return response.text
+    return None
 
 
-def update_ddns(ip):
-    client = boto3.client('route53')
+def update_ddns(ip: str) -> dict:
+    """Update the Route53 A record with the current IP.
+
+    Args:
+        ip: The IP address to set.
+
+    Returns:
+        The Route53 API response.
+    """
+    client = boto3.client("route53")
     response = client.change_resource_record_sets(
         ChangeBatch={
-            'Changes': [
+            "Changes": [
                 {
-                    'Action': 'UPSERT',
-                    'ResourceRecordSet': {
-                        'Name': 'eth.forcepu.sh',
-                        'ResourceRecords': [
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": "eth.forcepu.sh",
+                        "ResourceRecords": [
                             {
-                                'Value': ip,
+                                "Value": ip,
                             },
                         ],
-                        'TTL': TTL,
-                        'Type': 'A',
+                        "TTL": TTL,
+                        "Type": "A",
                     },
                 },
             ],
-            'Comment': 'DDNS',
+            "Comment": "DDNS",
         },
-        HostedZoneId=os.environ['HOSTED_ZONE'],
+        HostedZoneId=os.environ["HOSTED_ZONE"],
     )
     return response
 
 
-while True:
-    ip = get_ip()
-    if ip:
-        update_ddns(ip)
-
-    sleep(TTL)
+if __name__ == "__main__":
+    while True:
+        current_ip = get_ip()
+        if current_ip:
+            update_ddns(current_ip)
+        sleep(TTL)
