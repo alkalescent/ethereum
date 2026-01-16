619d644966be7948c35f652f73492638200e201e
Krish Suchak <suchak.krish@gmail.com>
2026-01-16 10:12:54 -0500

    chainsafe

--- scripts/checkpoint.sh
+++ scripts/checkpoint.sh
@@ -2,25 +2,19 @@
 
 set -eu
 
-# Checkpoint sync hosts (ethpandaops - official testnet provider)
-# Hoodi: https://checkpoint-sync.hoodi.ethpandaops.io
-# Mainnet: https://sync.invis.tools (ethpandaops doesn't run mainnet)
-
-# ethstaker.cc alternatives (may have issues post-Fusaka):
-# Hoodi: https://hoodi.beaconstate.ethstaker.cc
-# Mainnet: https://beaconstate.ethstaker.cc
+# Checkpoint sync hosts (ChainSafe/Lodestar - consistent provider for both networks)
+# Hoodi: https://beaconstate-hoodi.chainsafe.io
+# Mainnet: https://beaconstate-mainnet.chainsafe.io
 
 # Default deploy env for app code should be prod
 DEPLOY_ENV="${DEPLOY_ENV:-prod}"
 
 if [[ "${DEPLOY_ENV}" = "dev" ]]
 then
-    # Hoodi testnet - ethpandaops is official provider
-    NODE_HOST="https://checkpoint-sync.hoodi.ethpandaops.io"
+    NODE_HOST="https://beaconstate-hoodi.chainsafe.io"
     curl -LO https://github.com/eth-clients/hoodi/raw/main/metadata/genesis.ssz
 else
-    # Mainnet - use ethstaker.cc (reliable, community maintained)
-    NODE_HOST="https://beaconstate.ethstaker.cc"
+    NODE_HOST="https://beaconstate-mainnet.chainsafe.io"
 fi
 
 prysmctl checkpoint-sync download --beacon-node-host="${NODE_HOST}"
